4aa8f46cb6ba094a5172f85e44ff8e1fe42864fa
Alexandre MOTTIER <am@am-networks.fr>
2023-01-29 01:47:15 +0100

    Personnalisation nom affichage via fichier config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Saisissez les informations vous concernant (prénom, nom et poste recherché)
+// ATTENTION : BIEN ÉCRIRE CES INFORMATIONS ENTRE LES GUILLEMETS !
+$prenom = "Alexandre";
+$nom = "MOTTIER";
+$poste = "Administrateur Systèmes et Réseaux";
+
+?>
--- index.php
+++ index.php
@@ -5,8 +5,13 @@
   <meta name="viewport" content="width=device-width">
 </head>
 <body>
-    <h1>Utilitaire PHP de Recherche d'Emploi</h1>
-    <h2>Liste des entreprises</h2>
+<?php include("config.php") ?>
+  <fieldset>
+    <legend><font size="5"><?php echo $prenom . " " . $nom ?></font></legend>
+    <font size="4"><?php echo $poste ?></font><br>
+  </fieldset><br><br>
+    <font size="5">Votre recherche d'emploi - Liste des entreprises</font>
+
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -15,7 +20,7 @@ error_reporting(E_ALL);
 include("class/sqlconnect.php");
 
 // Récupère les données de la table Entreprise
-$sql = "SELECT Entreprise.ID AS EntrepriseID, Entreprise.NomSociete, Contact.ID, Contact.Prenom, Contact.Nom, Entreprise.Adresse, StatutEntretien.Statut
+$sql = "SELECT Entreprise.ID AS EntrepriseID, Entreprise.NomSociete, Contact.ID, Contact.Prenom, Contact.Nom, Contact.Mobile, Entreprise.Adresse, StatutEntretien.Statut
 FROM Entreprise
 LEFT JOIN Contact ON Entreprise.ID = Contact.IDEntreprise
 LEFT JOIN StatutEntretien ON Entreprise.StatutEntretien = StatutEntretien.ID";
@@ -29,7 +34,7 @@ echo "<tr><th style='border:1px solid #000;'>Nom de la société</th><th style='
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
     echo "<tr>";
     echo "<td style='border:1px solid #000;'>" . "<a href='modifentreprise.php?ID=" . $row['EntrepriseID'] . "'>" . $row['NomSociete'] . "</td>";
-    echo "<td style='border:1px solid #000;'>" . "<a href='infocontact.php?ID=" . $row['ID'] . "'>" . $row['Prenom'] . ' ' . $row['Nom'] . "</a>" . "</td>";
+    echo "<td style='border:1px solid #000;'>" . "<a href='infocontact.php?ID=" . $row['ID'] . "'>" . $row['Prenom'] . ' ' . $row['Nom'] . ' (' . $row['Mobile'] . ")</a>" . "</td>";
     echo "<td style='border:1px solid #000;'>" . $row['Adresse'] . "</td>";
     echo "<td style='border:1px solid #000;'>" . $row['Statut'] . "</td>";
     echo "<td style='border:1px solid #000;'>" . "<a href='infoentretien.php?ID=" . $row['ID'] . "'>Afficher les entretiens</a>" . "</td>";
@@ -42,10 +47,9 @@ mysqli_close($conn);
 <button onclick="window.location.href='ajoutentreprise.php'" class="button">Créer une entreprise</button>
 <button onclick="window.location.href='ajoutcontact.php'" class="button">Créer un contact</button>
 <button onclick="window.location.href='modificationstatut.php'" class="button">Modifier le statut pour une entreprise</button>
+<button onclick="window.location.href='creationentretientel.php'" class="button">Saisir un entretien téléphonique</button>
+<button onclick="window.location.href='creationentretienphy.php'" class="button">Saisir un entretien présentiel ou visio</button>
 <br><br>
-<a href="creationentretientel.php">Saisir un entretien téléphonique</a><br>
-<a href="creationentretienphy.php">Saisir un entretien présentiel/visio</a>
-
 <footer>
 <a href="https://github.com/alexandremottier/PHP-RechercheEmploi" target="_blank">Made with love by Aiden ♥️</a>
 </footer>
