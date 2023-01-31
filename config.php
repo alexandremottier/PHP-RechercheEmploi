d3f4ce00b0471e0515d03d7f219ed0a25e1509a0
Runard <40338703+alexandremottier@users.noreply.github.com>
2023-01-31 14:05:33 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Saisissez les informations vous concernant (prénom, nom et poste recherché)
-// ATTENTION : BIEN ÉCRIRE CES INFORMATIONS ENTRE LES GUILLEMETS !
-$prenom = "Alexandre";
-$nom = "MOTTIER";
-$poste = "Administrateur Systèmes et Réseaux";
-
-?>
